ef12dda9ee842950d8f155a49cab8e58d7c41d0c
Joshua Pelton-Stroud <jpstroud@gmail.com>
2026-08-13 16:50:39 -0400

    feat(firebase): codify security rules, indexes, and add automated emulator test target

--- apps/server-firebase/vitest.config.ts
+++ apps/server-firebase/vitest.config.ts
@@ -15,8 +15,10 @@ export default defineConfig({
     include: [
       'apps/server-firebase/src/**/*.spec.ts',
       'apps/server-firebase/src/**/*.test.ts',
+      'libs/server/**/*.spec.ts',
       'libs/server-firebase/**/*.spec.ts',
     ],
+    passWithNoTests: true,
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html', 'lcov'],
