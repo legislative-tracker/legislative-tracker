d2a7eab742055d5ad1d010f1be20df43a2277bc7
Joshua Pelton-Stroud <jpstroud@gmail.com>
2026-08-14 12:35:45 -0400

    feat(server-firebase): auto update legislator sponsorships on bill write and enforce canonical bill ID

--- libs/server/triggers-firebase/src/lib/legislation/index.ts
+++ libs/server/triggers-firebase/src/lib/legislation/index.ts
@@ -3,3 +3,6 @@ export * from './remove-bill';
 export * from './nightly-update';
 export * from './manual-update';
 export * from './service';
+export * from './sync-sponsorships';
+export * from './on-bill-written';
+
