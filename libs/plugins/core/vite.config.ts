379f4acbd2e9b9357835ed19564a72a854c354cb
Joshua Pelton-Stroud <jpstroud@gmail.com>
2026-08-14 07:49:53 -0400

    chore(tooling): migrate to Node 24 and modernize Nx Vitest plugins

--- libs/plugins/core/vite.config.ts
+++ libs/plugins/core/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig(() => ({
   root: import.meta.dirname,
   cacheDir: '../../../node_modules/.vite/libs/plugins/core',
-  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [tsconfigPaths(), nxCopyAssetsPlugin(['*.md'])],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
