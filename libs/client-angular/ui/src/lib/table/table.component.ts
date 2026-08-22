de133659b733ceb140a40dfdc5c14c5b45bf2b51
Joshua Pelton-Stroud <jpstroud@gmail.com>
2026-08-22 14:50:55 -0400

    fix(client-angular): use ocd-person routeType for dashboard member tables

--- libs/client-angular/ui/src/lib/table/table.component.ts
+++ libs/client-angular/ui/src/lib/table/table.component.ts
@@ -40,9 +40,7 @@ export class TableComponent<T> {
   dataSource = input.required<T[]>();
   columnSource = input.required<ColumnConfig<T>[]>();
   stateCd = input.required<string>(); // Passed from parent or route
-  routeType = input.required<
-    'bill' | 'member' | 'legislation' | 'ocd-person'
-  >();
+  routeType = input.required<'bill' | 'legislation' | 'ocd-person'>();
   chamber = input<'SENATE' | 'ASSEMBLY'>();
   defaultSortKeyInput = input<string | undefined>(undefined, {
     alias: 'defaultSortKey',
